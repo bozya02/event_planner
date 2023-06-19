2296e30ac6267358d917d43ece25a0fab9ace4c6
bozya02 <bozinandrey1@yandex.ru>
2023-06-19 11:52:34 +0300

    add creating task from event page

--- frontend/urls.py
+++ frontend/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('overview/add_employees/', views.overview_view, name='add_event_task'),
     path('event/<int:event_id>/add_employees/', views.event_view, name='add_employees'),
     path('event/<int:event_id>/add_responsible/', views.event_view, name='add_responsible'),
+    path('event/<int:event_id>/add_event_task/', views.event_view, name='event_add_event_task'),
     path('events/new/', views.new_event_view, name='new_event'),
     path('reports/', views.reports_view, name='reports'),
     path('employees/', views.employees_view, name='employees'),
@@ -23,5 +24,5 @@ urlpatterns = [
     path('employees/<int:employee_id>/edit/', views.employee_view, name='edit_employee'),
     path('employees/new/', views.new_employee_view, name='new_employee'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-path('check_task/<int:task_id>', views.change_task_state, name='change_task_state'),
+    path('check_task/<int:task_id>', views.change_task_state, name='change_task_state'),
 ]
